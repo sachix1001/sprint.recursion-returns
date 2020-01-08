9e05b4c20a7a2bbded8a2c325b6288ecf02b2dd7
Sachi Grannan <sachix1001@gmail.com>
2020-01-08 22:01:56 +0900

    pass the test for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,27 +4,23 @@ const getElementsByClassName = (targetName) => {
 
   function checkNode(element) {
     // base case
-    if (element.children.length === 0 || element.children === undefined) {
+    if (element.children.length === 0) {
+      if (element.classList.contains(targetName)) {
+        arrEl.push(element);
+      }
       return;
     }
 
     // check if element contains targetName
-    if (element.classList) {
-      if (element.classList.contains(targetName)) {
-        arrEl.push(element);
-      }
-      // console.log("1 element.children: ", element.children);
-      // console.log("2 child: ", element.children[i]);
-      // console.log("3", element.className);
-      // console.log(element.classList.contains(targetName));
-      for (let i = 0; i < element.children.length; i++) {
-        checkNode(element.children[i]);
-      }
+    if (element.classList.contains(targetName)) {
+      arrEl.push(element);
+    }
+    for (let i = 0; i < element.children.length; i++) {
+      checkNode(element.children[i]);
     }
   }
 
   checkNode(document.body);
-  console.log(arrEl);
   return arrEl;
 };
 
