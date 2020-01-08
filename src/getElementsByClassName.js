c65416b1bfdd5ba6af09bbb67f99d3e2dd250169
Sachi Grannan <sachix1001@gmail.com>
2020-01-09 07:23:02 +0900

    chenge getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,18 +3,10 @@ const getElementsByClassName = (targetName) => {
   const arrEl = [];
 
   function checkNode(element) {
-    // base case
-    if (element.children.length === 0) {
-      if (element.classList.contains(targetName)) {
-        arrEl.push(element);
-      }
-      return;
-    }
-
-    // check if element contains targetName
     if (element.classList.contains(targetName)) {
       arrEl.push(element);
     }
+
     for (let i = 0; i < element.children.length; i++) {
       checkNode(element.children[i]);
     }
